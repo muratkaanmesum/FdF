e8dced4b03db4fb47ee2f60c624ee888c40359e3
MKM327 <kaanmesum@gmail.com>
2022-12-26 22:17:52 +0300

    t

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int	main(int argc, char **argv)
 	map = get_map(argv[1]);
 	check_map(map);
 	mlx->mlx = mlx_init();
-	mlx->mlx_win = mlx_new_window(mlx->mlx, 1920, 1080, "Hello world!");
+	mlx->mlx_win = mlx_new_window(mlx->mlx, 1000, 1000, "Hello world!");
 	mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, render_map(map, mlx)->img,
 			0, 0);
 	mlx_loop(mlx->mlx);
--- utils.c
+++ utils.c
@@ -1,5 +1,16 @@
 #include "fdf.h"
+void	free_split(char **split)
+{
+	int	i;
 
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
 int	does_include(char *str, char c)
 {
 	int	i;
